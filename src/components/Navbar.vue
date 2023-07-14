1c0b33ff08d2c27acae4d29f837afa745805ba29
Toriola <faaedah@gmail.com>
2023-07-14 10:15:33 +0100

    REFACTOR: ran linting command, for formatting

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -1,33 +1,33 @@
 <template>
-    <nav class="logo-wrap" :style="{ 'background-color': backgroundColor }">
-        <RouterLink to="/">
-            <span class="bold">PAW</span>
-            <span>finder</span>
-        </RouterLink>
-    </nav>
+  <nav class="logo-wrap" :style="{ 'background-color': backgroundColor }">
+    <RouterLink to="/">
+      <span class="bold">PAW</span>
+      <span>finder</span>
+    </RouterLink>
+  </nav>
 </template>
 
 <script setup>
 import { RouterLink } from 'vue-router'
 defineProps({
-    backgroundColor: {
-        type: String,
-        default: 'transparent'
-    }
+  backgroundColor: {
+    type: String,
+    default: 'transparent'
+  }
 })
 </script>
 
 <style scoped>
-   .logo-wrap {
-       padding: 1rem;
-   }
+.logo-wrap {
+  padding: 1rem;
+}
 
-   .logo-wrap a {
-        color: var(--light);    
-   }
+.logo-wrap a {
+  color: var(--light);
+}
 
-   .bold {
-        font-weight: 500;
-        font-size: 1.6rem;
-    }
+.bold {
+  font-weight: 500;
+  font-size: 1.6rem;
+}
 </style>
--- src/components/TheHeader.vue
+++ src/components/TheHeader.vue
@@ -1,72 +1,71 @@
 <template>
-    <header class="home-header">
-        <div>
-            <Navbar />
-            <div class="content">
-                <h1>Dive into Dogdom!</h1>
-                <p>Explore Images and Profiles Of Dogs.</p>
-            </div>
-        </div>
-    </header>
+  <header class="home-header">
+    <div>
+      <Navbar />
+      <div class="content">
+        <h1>Dive into Dogdom!</h1>
+        <p>Explore Images and Profiles Of Dogs.</p>
+      </div>
+    </div>
+  </header>
 </template>
 
 <script setup>
-import Navbar from './Navbar.vue';
+import Navbar from './Navbar.vue'
 </script>
 
 <style scoped>
 .home-header {
-    background-image: url('@/assets/images/bg-header.jpg');
-    background-color: var(--primary);
-    height: 300px;
-    background-position: center; 
-    background-repeat: no-repeat;
-    background-size: cover; 
-    position: relative;
+  background-image: url('@/assets/images/bg-header.jpg');
+  background-color: var(--primary);
+  height: 300px;
+  background-position: center;
+  background-repeat: no-repeat;
+  background-size: cover;
+  position: relative;
 }
 
 .home-header::after {
-    content: '';
-    background: rgba(0, 0, 0, 0.4);
-    position: absolute;
-    top: 0;
-    right: 0;
-    bottom: 0;
-    left: 0;
-    z-index: 1;
+  content: '';
+  background: rgba(0, 0, 0, 0.4);
+  position: absolute;
+  top: 0;
+  right: 0;
+  bottom: 0;
+  left: 0;
+  z-index: 1;
 }
 
 .home-header > div {
-    position: relative;
-    z-index: 10;
+  position: relative;
+  z-index: 10;
 }
 
 .content {
-    padding: 5rem 1rem 0;
+  padding: 5rem 1rem 0;
 }
 
 .home-header h1,
 .home-header p {
-    text-align: center;
-    color: var(--light);
+  text-align: center;
+  color: var(--light);
 }
 
-.home-header p { 
-    font-size: .85rem;
+.home-header p {
+  font-size: 0.85rem;
 }
 
 @media screen and (min-width: 700px) {
-    .home-header {
-        height: 350px;
-    }
+  .home-header {
+    height: 350px;
+  }
 
-    .home-header h1 {
-        font-size: 3rem;
-    }
+  .home-header h1 {
+    font-size: 3rem;
+  }
 
-    .home-header p {
-        font-size: 1rem;
-    }
+  .home-header p {
+    font-size: 1rem;
+  }
 }
-
 </style>
