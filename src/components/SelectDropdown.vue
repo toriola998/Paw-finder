0a695f146edb5d5951645fb7998586ca3350825a
Toriola <faaedah@gmail.com>
2023-07-15 00:22:28 +0100

    ADD: integrated 'get breed list' endpoint

--- src/components/SelectDropdown.vue
+++ src/components/SelectDropdown.vue
@@ -9,7 +9,7 @@
       aria-controls="custom-dropdown-options"
       @click="toggleDropdown"
     >
-      <span>Search dogs by breed</span>
+      <span>{{ fetchingMessage }}</span>
       <img
         src="@/assets/images/icon-dropdown.png"
         alt=""
@@ -25,20 +25,69 @@
       aria-labelledby="custom-dropdown-label custom-dropdown-button"
       aria-hidden="true"
     >
-      <li role="option" aria-selected="false">Beignj</li>
-      <li role="option" aria-selected="false">Rotweiler</li>
-      <li role="option" aria-selected="false">Option 3</li>
+      <li
+        role="option"
+        aria-selected="false"
+        v-for="(item, index) in breedList"
+        :key="index"
+        @click="getBreed(item)"
+      >
+        {{ item }}
+      </li>
     </ul>
   </div>
 </template>
 
 <script setup>
-import { ref } from 'vue'
+import { ref, onMounted } from 'vue'
 const showDropdown = ref(false)
 
 function toggleDropdown() {
   showDropdown.value = !showDropdown.value
 }
+
+const fetchingData = ref(false)
+const fetchingMessage = ref('')
+const breedList = ref([])
+const breed = ref('')
+
+function getBreedList() {
+  fetch('https://dog.ceo/api/breeds/list/all')
+    .then((res) => {
+      if (res.status === 200) {
+        return res.json()
+      } else {
+        fetchingMessage.value = 'Failed to get breeds'
+      }
+    })
+    .then((data) => {
+      let responseObject = data.message
+      for (const key in responseObject) {
+        if (Array.isArray(responseObject[key])) {
+          // Push the keys into the new array
+          breedList.value.push(key)
+        }
+      }
+      console.log(breedList.value)
+      fetchingMessage.value = 'Search dogs by breed'
+    })
+    .catch((err) => {
+      console.log(err)
+      fetchingMessage.value = 'Failed to get breeds'
+    })
+    .finally(() => {
+      fetchingData.value = false
+    })
+}
+
+function getBreed(arg) {
+  breed.value = arg
+  console.log(breed.value)
+}
+
+onMounted(() => {
+  getBreedList()
+})
 </script>
 
 <style scoped>
@@ -52,6 +101,8 @@ function toggleDropdown() {
   position: absolute;
   width: 92%;
   margin-top: 1rem;
+  height: 350px;
+  overflow-y: scroll;
 }
 
 .custom-dropdown ul,
