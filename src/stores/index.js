d6160ad6b9deb26d63de5d951787e9af95ceb2a8
Toriola <faaedah@gmail.com>
2023-07-15 12:11:42 +0100

    REFACTOR: changed to 'dogapi' endpoint

--- src/stores/index.js
+++ src/stores/index.js
@@ -1,31 +1,27 @@
 import { ref } from 'vue'
 import { defineStore } from 'pinia'
+import axios from "axios";
+
+const api = axios.create({
+  baseURL: import.meta.env.VITE_BASE_URL,
+  headers: {'x-api-key': import.meta.env.VITE_SECRET_KEY}
+});
 
 export const useDogDataStore = defineStore('dogData', () => {
   let dataList = ref([])
   const fetchingData = ref(true)
 
-  function getData() {
+  async function getData() {
     fetchingData.value = true
-    fetch('https://dog.ceo/api/breeds/image/random/3')
-      .then((res) => {
-        if (res.status === 200) {
-          return res.json() // Return the promise
-        } else {
-          console.log('Something went wrong')
-        }
-      })
-      .then((data) => {
-        dataList.value = data.message
-        console.log(data, dataList.value)
-      })
-      .catch((err) => {
-        console.log(err)
-      })
-      .finally(() => {
-        fetchingData.value = false
-      })
+    try {
+      const response = await api.get('/images/search?limit=100');
+      dataList.value = response.data;
+      console.log(dataList.value);
+    } catch (error) {
+      console.error(error);
+    } finally {
+      fetchingData.value = false
+    }
   }
-
   return { getData, dataList, fetchingData }
 })
